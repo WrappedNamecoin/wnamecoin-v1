dee52bde64ae2b7d8c6bd96bb764e93da4434e27
Abid Omar <contact@omarabid.com>
2024-10-10 16:28:49 +0800

    chore(upgrade): update solidity version to 0.8.27

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,7 +22,7 @@ if (!process.env.ETHERSCAN_API_KEY) {
 
 // Hardhat Config
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: "0.8.27",
   networks: {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
